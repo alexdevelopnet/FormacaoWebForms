a7dd32e0fb88b2bed442e353cbc54659b7d69e98
alexdevelopnet <alexfnet@gmail.com>
2023-04-09 12:30:06 -0300

    Atualização, exclusão e criação de dados

---
+++ ContosoUniversityModelBinding/AddStudent.aspx.cs
@@ -0,0 +1,43 @@
+﻿using ContosoUniversityModelBinding.Data;
+using ContosoUniversityModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ContosoUniversityModelBinding
+{
+    public partial class AddStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void addStudentForm_InsertItem()
+        {
+            var item = new Student();
+
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                using (SchoolContext db = new SchoolContext())
+                {
+                    db.Students.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        protected void cancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/alunos");
+        }
+
+        protected void addStudentForm_ItemInserted(object sender, FormViewInsertedEventArgs e)
+        {
+            Response.Redirect("~/Students");
+        }
+    }
+}
--- ContosoUniversityModelBinding/Alunos.aspx.cs
+++ ContosoUniversityModelBinding/Alunos.aspx.cs
@@ -3,6 +3,7 @@ using ContosoUniversityModelBinding.Models;
 using System;
 using System.Linq;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversityModelBinding
 {
@@ -20,5 +21,44 @@ namespace ContosoUniversityModelBinding
              var query = db.Students.Include(s => s.Enrollments.Select(e => e.Course));
             return (IQueryable<Student>)query;
         }
+        public void studentsGrid_UpdateItem(int studentID)
+        {
+            using (SchoolContext db = new SchoolContext())
+            {
+                Student item = null;
+                item = db.Students.Find(studentID);
+                if (item == null)
+                {
+                    ModelState.AddModelError("",
+                      String.Format("Item with id {0} was not found", studentID));
+                    return;
+                }
+
+                TryUpdateModel(item);
+                if (ModelState.IsValid)
+                {
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public void studentsGrid_DeleteItem(int studentID)
+        {
+            using (SchoolContext db = new SchoolContext())
+            {
+                var item = new Student { StudentID = studentID };
+                db.Entry(item).State = EntityState.Deleted;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    ModelState.AddModelError("",
+                      String.Format("Item with id {0} no longer exists in the database.", studentID));
+                }
+            }
+        }
+
     }
 }
